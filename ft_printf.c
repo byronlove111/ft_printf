b18c9f02f82a9010ecd96221f39350ed0f686026
Abd'al-Malik Bourassi <malik.chan94420@gmail.com>
2024-11-13 03:21:40 +0100

    new p

--- ft_printf.c
+++ ft_printf.c
@@ -11,33 +11,85 @@
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
+#include <stdarg.h>
 
-int	if_statement(char c)
+void	ft_putchar_fd(char c, int fd)
 {
-	if (c == 'd' || c == 'i' || c == 
+	write(fd, &c, 1);
+}
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	if (!s)
+		return ;
+	while (*s)
+	{
+		ft_putchar_fd(*s, fd);
+		s++;
+	}
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n == -2147483648)
+	{
+		ft_putstr_fd("-2147483648", fd);
+		return ;
+	}
+	if (n < 0)
+	{
+		n = -n;
+		ft_putchar_fd('-', fd);
+	}
+	if (n > 9)
+		ft_putnbr_fd(n / 10, fd);
+	ft_putchar_fd((n % 10) + 48, fd);
+}
+
+int	if_statement(char c, va_list args)
+{
+	int	count;
+
+	count = 0;
+	if (c == 'c')
+		ft_putchar_fd(va_arg(args, int), 1);
+	else if (c == 's')
+		ft_putstr_fd(va_arg(args, char *), 1);
+	else if (c == 'i' || c == 'd')
+		ft_putnbr_fd(va_arg(args, int), 1);
+	else if (c == '%')
+		ft_putchar_fd('%', 1);
 }
 
 int	ft_printf(const char *str, ...)
 {
 	va_list	args;
+	int		counter;
 
 	va_start(args, str);
 	while (*str)
 	{
 		if (*str == '%')
 		{
-			if_statement(str + 1, args);	
+			str++;
+			if_statement(*str++, args);
 		}
-		write(1, str, 1);
-		str++;
+		else
+			write(1, str++, 1);
 	}
 	return (1);
 }
 
 #include <stdio.h>
+#include <limits.h>
+
 int	main(void)
 {
-	ft_printf("malik %s");
-	printf("malik %si", "lol");
-
+	char c = 'z';
+	char *str = " et luna";
+	int nb = INT_MIN;
+	int nb2 = INT_MAX;
+	ft_printf("%d%%%c%s%%",INT_MIN, c, str);
+	// printf("%%");
 }
